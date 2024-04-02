090130c7f7dde0a4991271afab0c5ef0c1395268
Edonak <edouanakasila69@gmail.com>
2024-04-02 11:36:26 +0100

    Mis en place d'un server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+//npm modules
+const express = require('express');
+
+// create the server
+const app = express();
+
+// create the homepage route at '/'
+app.get('/', (req, res) => {
+  console.log(req)
+  res.send('You hit the home page without restarting the server automatically\n')
+})
+
+// tell the server what port to listen on
+app.listen(3000, () => {
+  console.log('Listening on localhost:3000')
+})
